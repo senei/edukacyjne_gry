bfee32085766e3948e5f7f406a61ca11741dd374
senei master <senei@senei.local>
2013-02-05 13:38:59 +0100

    next version

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,14 +25,7 @@
   // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
   // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
   // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
-  $('#search .postFix').click(function(){
-    var value = $('#search input').val();
-    console.log(value);
-    $('.game').addClass('hide').filter(function(){
-      return $.trim($(this).text()).indexOf(value) != -1;
-    }).removeClass('hide');
 
-  })
   // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
   if (Modernizr.touch && !window.location.hash) {
     $(window).load(function () {
